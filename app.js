893de80206c14328e83a02e1ec670d1f358cae41
Adarsha <vatraiadarsh@gmail.com>
2022-07-29 12:31:49 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi ✌️!');
+});
+
+app.listen(3000, () => {
+    console.log('Express app listening on port 3000!');
+});
