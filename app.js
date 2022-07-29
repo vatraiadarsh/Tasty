4797a530bf38d9acd99b842d8f104fd732d97626
Adarsha <vatraiadarsh@gmail.com>
2022-07-29 15:46:40 +1000

    refactoring

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import morgan from 'morgan';
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(morgan('dev'));
 
+// db connection
+
+import { mongoose } from 'mongoose';
+try {
+    const conn = await mongoose.connect('mongodb://localhost:27017/mvc',);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+} catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+}
 
 app.get('/', (req, res) => {
     res.send('Hi ✌️!');
